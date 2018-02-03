34f4594569d43387dfceb17be0f55f26947ca9da
袁佳阳 <yuanjiayang@qianshengqian.com>
2018-02-03 20:37:25 +0800

    修改线上 文件上传目录

--- src/main/java/com/lxl/common/consts/CommonConst.java
+++ src/main/java/com/lxl/common/consts/CommonConst.java
@@ -3,6 +3,9 @@ package com.lxl.common.consts;
 public class CommonConst {
     public static final String DATABASE_NAME = "lxlmanage";
 
-    public static final String FILE_BASE_HOST = "http://localhost/upload/";
-    public static final String FILE_BASE_DIR = "/Users/yuanjiayang/tmp/www/upload/";
+    //public static final String FILE_BASE_HOST = "http://localhost/upload/";
+    //public static final String FILE_BASE_DIR = "/Users/yuanjiayang/tmp/www/upload/";
+
+    public static final String FILE_BASE_HOST = "http://122.112.216.51/upload/";
+    public static final String FILE_BASE_DIR = "/var/www/community-vue/dist/upload/";
 }
